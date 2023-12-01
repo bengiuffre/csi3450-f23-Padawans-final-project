accdfdc131b8b977119e5dedb6e88185255f6f52
brendancrabill <98237875+brendancrabill@users.noreply.github.com>
2023-11-30 19:16:00 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEC System</title>
+</head>
+<body>
+    <h1>Temporary Employment Corporation (TEC) System</h1>
+    <ul>
+        <li><a href="candidates.php">Candidates</a></li>
+        <!-- Add links for other sections if needed -->
+    </ul>
+</body>
+</html>
