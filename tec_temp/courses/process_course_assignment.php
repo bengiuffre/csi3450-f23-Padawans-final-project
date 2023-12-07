98aaf2f77833e6d340aa2677981fa14bf33e2676
brendancrabill <98237875+brendancrabill@users.noreply.github.com>
2023-12-06 20:24:02 -0500

    Update process_course_assignment.php

--- tec_temp/courses/process_course_assignment.php
+++ tec_temp/courses/process_course_assignment.php
@@ -5,29 +5,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $candidate_id = $_POST['candidate_id'];
     $course_code = $_POST['course_code'];
 
-    // Check if the candidate ID exists in the 'candidate' table
+    // Check if the candidate ID already exists
     $checkCandidateQuery = "SELECT COUNT(*) AS candidate_count FROM candidate WHERE candidate_id = '$candidate_id'";
     $resultCandidate = mysqli_query($conn, $checkCandidateQuery);
     $rowCandidate = mysqli_fetch_assoc($resultCandidate);
     $candidateCount = $rowCandidate['candidate_count'];
 
     if ($candidateCount > 0) {
-        // Check if the course code exists in the 'course' table
+        // Check if the course code exists
         $checkCourseQuery = "SELECT qualification_code FROM course WHERE course_code = '$course_code'";
         $resultCourse = mysqli_query($conn, $checkCourseQuery);
 
         if ($rowCourse = mysqli_fetch_assoc($resultCourse)) {
             $qualification_code = $rowCourse['qualification_code'];
 
-            // Insert the pair (candidate_id, qualification_code) into the 'candidate_qualification' table
+            // Insert candidate_id and qualification_code into the candidate_qualification table
             $insertQualificationQuery = "INSERT INTO candidate_qualification (candidate_id, qualification_code) VALUES ('$candidate_id', '$qualification_code')";
             if (mysqli_query($conn, $insertQualificationQuery)) {
-                echo "Qualification assigned successfully! ";
+                echo "Qualification assigned";
                 
                 // Insert the pair (candidate_id, course_code) into the 'candidate_course_assignment' table
                 $insertCourseAssignmentQuery = "INSERT INTO candidate_course_assignment (candidate_id, course_code) VALUES ('$candidate_id', '$course_code')";
                 if (mysqli_query($conn, $insertCourseAssignmentQuery)) {
-                    echo "Course assigned successfully!";
+                    echo "Course assigned";
                 } else {
                     echo "Error: " . $insertCourseAssignmentQuery . "<br>" . mysqli_error($conn);
                 }
@@ -35,10 +35,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 echo "Error: " . $insertQualificationQuery . "<br>" . mysqli_error($conn);
             }
         } else {
-            echo "Error: The specified course code does not exist.";
+            echo "Error: The course code does not exist.";
         }
     } else {
-        echo "Error: The specified candidate ID does not exist.";
+        echo "Error: The candidate ID does not exist.";
     }
 
     // Close the database connection
