94db253b8dbd961655a345896711bd37cb7d79a3
brendancrabill <98237875+brendancrabill@users.noreply.github.com>
2023-12-06 20:26:28 -0500

    Update remove_company.php

--- tec_temp/company/remove_company.php
+++ tec_temp/company/remove_company.php
@@ -4,26 +4,25 @@ include_once 'db_connection.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $company_id = $_POST['company_id'];
 
-    // Check if the company ID exists in the 'company' table
+    // Check if the company ID already exists
     $checkCompanyQuery = "SELECT COUNT(*) AS company_count FROM company WHERE company_id = '$company_id'";
     $resultCompany = mysqli_query($conn, $checkCompanyQuery);
     $rowCompany = mysqli_fetch_assoc($resultCompany);
     $companyCount = $rowCompany['company_count'];
 
     if ($companyCount > 0) {
-        // Remove the company from the 'company' table
+        // Remove the company from the the respective table
         $removeCompanyQuery = "DELETE FROM company WHERE company_id = '$company_id'";
         
         if (mysqli_query($conn, $removeCompanyQuery)) {
-            echo "Company removed successfully!";
+            echo "Company removed";
         } else {
             echo "Error: " . $removeCompanyQuery . "<br>" . mysqli_error($conn);
         }
     } else {
-        echo "Error: The specified company ID does not exist.";
+        echo "Error: That company ID does not exist.";
     }
 
-    // Close the database connection
     mysqli_close($conn);
 }
 ?>
