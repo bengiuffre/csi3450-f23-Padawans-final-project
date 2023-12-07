e14f9d55f0ff36476b96b9c40d73528f0dbf5ecc
brendancrabill <98237875+brendancrabill@users.noreply.github.com>
2023-12-06 20:30:12 -0500

    Update process_registration.php
    revised

--- tec_temp/candidate/process_registration.php
+++ tec_temp/candidate/process_registration.php
@@ -5,16 +5,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $candidate_name = $_POST['candidate_name'];
     $job_details = $_POST['job_details'];
 
-    // Retrieve the current maximum candidate_id from the 'candidate' table
+    // Retrieve the current maximum existing candidate id, we then increment this by one for the next available candidate ID
     $maxIdQuery = "SELECT MAX(candidate_id) as max_id FROM candidate";
     $result = mysqli_query($conn, $maxIdQuery);
     $row = mysqli_fetch_assoc($result);
     $candidate_id = $row['max_id'] + 1;
 
-    // Insert candidate data into the 'candidate' table
+    // Insert candidate input into the candidate table
     $insertCandidateQuery = "INSERT INTO candidate (candidate_id, candidate_name) VALUES ('$candidate_id', '$candidate_name')";
     if (mysqli_query($conn, $insertCandidateQuery)) {
-        // Insert job history data into the 'job_history' table
+        // Insert job history into the job_history table
         $insertJobHistoryQuery = "INSERT INTO job_history (candidate_id, job_details) VALUES ('$candidate_id', '$job_details')";
         if (mysqli_query($conn, $insertJobHistoryQuery)) {
             echo "Candidate registered successfully!";
